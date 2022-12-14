f7b52402813cd14c1dab06c1b0718098674b277d
Tasuku Bobcorn <devbobcorn@outlook.com>
2022-12-14 17:02:23 +0800

    :ambulance: Fix entity palette for 1.19

--- Assets/Scripts/Mapping/EntityPalettes/EntityPalette.cs
+++ Assets/Scripts/Mapping/EntityPalettes/EntityPalette.cs
@@ -11,15 +11,6 @@ namespace MinecraftClient.Mapping.EntityPalettes
         protected abstract Dictionary<int, EntityType> GetDict();
 
         /// <summary>
-        /// Get mapping dictionary for pre-1.14 non-living entities.
-        /// </summary>
-        /// <returns>Palette dictionary for non-living entities (pre-1.14)</returns>
-        protected virtual Dictionary<int, EntityType> GetDictNonLiving()
-        {
-            return null;
-        }
-
-        /// <summary>
         /// Get entity type from type ID
         /// </summary>
         /// <param name="id">Entity type ID</param>
@@ -27,22 +18,12 @@ namespace MinecraftClient.Mapping.EntityPalettes
         public EntityType FromId(int id, bool living)
         {
             Dictionary<int, EntityType> entityTypes = GetDict();
-            Dictionary<int, EntityType> entityTypesNonLiving = GetDictNonLiving();
 
-            if (entityTypesNonLiving != null && !living)
-            {
-                //Pre-1.14 non-living entities have a different set of IDs (entityTypesNonLiving != null)
-                if (entityTypesNonLiving.ContainsKey(id))
-                    return entityTypesNonLiving[id];
-            }
-            else
-            {
-                //1.14+ entities have the same set of IDs regardless of living status
-                if (entityTypes.ContainsKey(id))
-                    return entityTypes[id];
-            }
+            //1.14+ entities have the same set of IDs regardless of living status
+            if (entityTypes.ContainsKey(id))
+                return entityTypes[id];
 
-            throw new System.IO.InvalidDataException("Unknown Entity ID " + id + ". Is Entity Palette up to date for this Minecraft version?");
+            throw new System.IO.InvalidDataException($"Unknown Entity ID {id} in palette {GetType()}. Is it up to date for this Minecraft version?");
         }
     }
 }
