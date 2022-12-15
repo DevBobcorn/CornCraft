edcb034c98af092f04960dcd8f310891ad9fe1e7
Tasuku Bobcorn <devbobcorn@outlook.com>
2022-12-15 20:34:14 +0800

    :sparkles: Add item lists for extra data

--- Assets/Scripts/Mapping/Item.cs
+++ Assets/Scripts/Mapping/Item.cs
@@ -2,20 +2,27 @@ namespace MinecraftClient.Mapping
 {
     public class Item
     {
-        public static readonly ResourceLocation AIR_ID = new ResourceLocation("air");
-        public static readonly Item UNKNOWN  = new Item(new("<missing_no>"));
-        public static readonly Item AIR_ITEM = new Item(new ResourceLocation("air"));
+        public const int DEFAULT_STACK_LIMIT = 64;
 
-        public readonly ResourceLocation itemId; // Something like 'minecraft:grass_block'
+        public static readonly ResourceLocation AIR_ID = new ResourceLocation("air");
+        public static readonly Item UNKNOWN  = new Item(new("<missing_no>")); // Unsupported item type (Forge mod custom item...)
+        public static readonly Item NULL     = new Item(new("<null_item>"));  // Unspecified item type (Used in the network protocol)
+        public static readonly Item AIR_ITEM = new Item(new ResourceLocation("air")) { StackLimit = 1 };
+
+        public readonly ResourceLocation ItemId; // Something like 'minecraft:grass_block'
+        public int StackLimit = DEFAULT_STACK_LIMIT;
+        public ItemRarity Rarity = ItemRarity.Common;
+
+        public bool IsStackable => StackLimit > 1;
 
         public Item(ResourceLocation itemId)
         {
-            this.itemId = itemId;
+            this.ItemId = itemId;
         }
 
         public override string ToString()
         {
-            return itemId.ToString();
+            return ItemId.ToString();
         }
     }
 }
