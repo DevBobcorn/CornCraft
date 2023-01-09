ae174f99da8c5ba267737b803b7c713b0ecae45a
Tasuku Bobcorn <DevBobcorn@outlook.com>
2023-01-09 17:47:51 +0800

    :recycle: Improve player controller

--- Assets/Scripts/Control/PlayerStatus.cs
+++ Assets/Scripts/Control/PlayerStatus.cs
@@ -16,7 +16,6 @@ namespace MinecraftClient.Control
         public bool Moving    = false;
         public bool Sprinting = false;
 
-        public float MovePersistence = 0F;
         public float BrakeTime       = 0F;
 
         public bool Attacking  = false;
@@ -42,7 +41,7 @@ namespace MinecraftClient.Control
         public Location? TargetBlockPos = null;
 
         public override string ToString() => 
-            $"InLiquid:\t{InLiquid}\nGrounded:\t{Grounded}\nOn Wall:\t{OnWall}\nMoving:\t{Moving}\t{CenterDownDist:0.00}\t{FrontDownDist:0.00}\t{LiquidDist:0.00}\nGrounded:\t{Sprinting}\nGround Slope:\t{GroundSlope:0.00}\nBarrier Angle:\t{BarrierAngle:0.00}\tDist:\t{BarrierDist:0.00}";
+            $"InLiquid:\t{InLiquid}\nGrounded:\t{Grounded}\nOn Wall:\t{OnWall}\nMoving:\t{Moving}\t{CenterDownDist:0.00}\t{FrontDownDist:0.00}\t{LiquidDist:0.00}\nGrounded:\t{Sprinting}\nGround Slope:\t{GroundSlope:0.00}\nBarrier Angle:\t{BarrierAngle:0.00}\tDist:\t{BarrierDist:0.00}\n\nBrake Time:\t{BrakeTime:0.00}";
 
     }
 }
