0b41ee960a5cec613957298a676eb5079995d5e5
Tasuku Bobcorn <DevBobcorn@outlook.com>
2023-02-05 17:30:34 +0800

    :heavy_minus_sign: Remove COZY

--- Assets/Scripts/Core/SceneObjectHolder.cs
+++ Assets/Scripts/Core/SceneObjectHolder.cs
@@ -5,8 +5,6 @@ using MinecraftClient.Control;
 using MinecraftClient.UI;
 using MinecraftClient.Rendering;
 
-using DistantLands.Cozy;
-
 namespace MinecraftClient
 {
     public class SceneObjectHolder : MonoBehaviour
@@ -18,9 +16,6 @@ namespace MinecraftClient
 
         // Camera Controller Object
         [SerializeField] public CameraController? cameraController;
-
-        // Cozy Weather Object
-        [SerializeField] public CozyWeather? cozyWeather;
 
         // World Render Manager Objects
         [SerializeField] public EntityRenderManager? entityRenderManager;
@@ -35,7 +30,6 @@ namespace MinecraftClient
             return screenControl is not null
                     && hudScreen is not null
                     && cameraController is not null
-                    // && cozyWeather is not null
                     && entityRenderManager is not null
                     && chunkRenderManager is not null
                     && playerPrefab is not null;
