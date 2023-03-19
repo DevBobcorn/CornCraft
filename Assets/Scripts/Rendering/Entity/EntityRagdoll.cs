bae868688e8ef65cd5a03f6237c8c4cb892073bd
Tasuku Bobcorn <DevBobcorn@outlook.com>
2023-03-19 18:08:50 +0800

    :alembic: More ragdolls

--- Assets/Scripts/Rendering/Entity/EntityRagdoll.cs
+++ Assets/Scripts/Rendering/Entity/EntityRagdoll.cs
@@ -6,12 +6,20 @@ namespace MinecraftClient.Rendering
     public class EntityRagdoll : MonoBehaviour
     {
         [SerializeField] public Rigidbody? mainRigidbody;
+        [SerializeField] protected Transform? visual;
+        public Transform Visual => visual!;
 
         void Start()
         {
             if (mainRigidbody == null)
             {
                 Debug.LogWarning("Ragdoll main rigidbody not assigned!");
+            }
+
+            if (visual == null)
+            {
+                Debug.LogWarning("Visual transform for entity render not assigned!");
+                visual = transform;
             }
             
         }
