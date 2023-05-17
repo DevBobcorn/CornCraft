c9c402c3cf8b8c0f1d4e52e7a975dda92afc3a3a
Tasuku Bobcorn <DevBobcorn@outlook.com>
2023-05-17 13:07:19 +0800

    :fire: Remove unused files and code

--- Assets/Scripts/Inventory/ContainerTypeExtensions.cs
+++ Assets/Scripts/Inventory/ContainerTypeExtensions.cs
@@ -38,54 +38,5 @@ namespace MinecraftClient.Inventory
                 default: return 0;
             }
         }
-
-        /// <summary>
-        /// Get an ASCII art representation of the container
-        /// </summary>
-        /// <param name="c"></param>
-        /// <returns>ASCII art representation or NULL if not implemented for this container type</returns>
-        public static string GetAsciiArt(this ContainerType c)
-        {
-            switch (c)
-            {
-                case ContainerType.PlayerInventory:
-                    return getTextOrNull("PlayerInventory");
-                case ContainerType.Generic_9x3:
-                    return getTextOrNull("Generic_9x3");
-                case ContainerType.Generic_9x6:
-                    return getTextOrNull("Generic_9x6");
-                case ContainerType.Generic_3x3:
-                    return getTextOrNull("Generic_3x3");
-                case ContainerType.Crafting:
-                    return getTextOrNull("Crafting");
-                case ContainerType.BlastFurnace: return null;
-                case ContainerType.Furnace: return null;
-                case ContainerType.Smoker: return null;
-                case ContainerType.Enchantment: return null;
-                case ContainerType.BrewingStand:
-                    return getTextOrNull("BrewingStand");
-                case ContainerType.Merchant: return null;
-                case ContainerType.Beacon: return null;
-                case ContainerType.Anvil: return null;
-                case ContainerType.Hopper: return null;
-                case ContainerType.ShulkerBox: return null;
-                case ContainerType.Loom: return null;
-                case ContainerType.Stonecutter: return null;
-                case ContainerType.Lectern: return null;
-                case ContainerType.Cartography: return null;
-                case ContainerType.Grindstone: return null;
-                case ContainerType.Unknown: return null;
-                default: return null;
-            }
-        }
-
-        private static string getTextOrNull(string name)
-        {
-            try {
-                return File.ReadAllText(Application.streamingAssetsPath + "/Containers/ContainerType." + name + ".txt");
-            } catch {
-                return null;
-            }
-        }
     }
 }
