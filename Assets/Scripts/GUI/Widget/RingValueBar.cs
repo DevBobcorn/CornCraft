6d531ded45429c68a246a2056153df269d1683e9
Tasuku Bobcorn <DevBobcorn@outlook.com>
2023-05-19 11:06:02 +0800

    :recycle: Refactor value bars

--- Assets/Scripts/GUI/Widget/RingValueBar.cs
+++ Assets/Scripts/GUI/Widget/RingValueBar.cs
@@ -4,95 +4,49 @@ using UnityEngine.UI;
 
 namespace MinecraftClient.UI
 {
-    public class RingValueBar : MonoBehaviour
+    public class RingValueBar : BaseValueBar
     {
-        [SerializeField] private Color normalColor   = Color.white;
-        [SerializeField] private float smoothSpeed   = 1000F;
-
+        [SerializeField] private Color normalColor  = Color.white;
         [SerializeField] private Color warningColor = Color.yellow;
         [SerializeField] private Color dangerColor  = Color.red;
 
-        [SerializeField] private float warningThreshold = 0.3F;
-        [SerializeField] private float dangerThreshold  = 0.1F;
+        [SerializeField] [Range(0.1F, 1F)] private float warningThreshold = 0.3F;
+        [SerializeField] [Range(0.1F, 1F)] private float dangerThreshold  = 0.1F;
 
         [SerializeField] private float fullBarDegree = 40F;
+        [SerializeField] [Range(500F, 5000F)] private float smoothSpeed = 1000F;
 
-        private Image? backgroundImage, fillImage;
-
-        private float maxValue = 100F, curValue = 100F, displayValue = 100F;
-
-        public float MaxValue
-        {
-            get {
-                return maxValue;
-            }
-
-            set { // Preserve old visual fill percentage
-                float oldFract = displayValue / maxValue; // old max value
-                maxValue = value;
-                displayValue = oldFract * maxValue; // new max value
-            }
-        }
-
-        public float CurValue
-        {
-            get {
-                return curValue;
-            }
-
-            set {
-                if (value < 0F)
-                    curValue = 0F;
-                else if (value > maxValue)
-                    curValue = maxValue;
-                else
-                    curValue = value;
-            }
-        }
+        [SerializeField] private Image? backgroundImage, fillImage;
 
         void Start()
         {
-            backgroundImage = FindHelper.FindChildRecursively(transform, "Bar Background").GetComponent<Image>();
-            fillImage = FindHelper.FindChildRecursively(transform, "Bar Fill").GetComponent<Image>();
-
-            backgroundImage.transform.rotation = Quaternion.AngleAxis(fullBarDegree / 2F, Vector3.back);
-            fillImage.transform.rotation = Quaternion.AngleAxis(fullBarDegree / 2F, Vector3.back);
+            backgroundImage!.transform.rotation = Quaternion.AngleAxis(fullBarDegree / 2F, Vector3.back);
+            fillImage!.transform.rotation = Quaternion.AngleAxis(fullBarDegree / 2F, Vector3.back);
 
             float displayFrac = displayValue / maxValue;
-
             backgroundImage.fillAmount = fullBarDegree / 360F;
-            fillImage.fillAmount = displayFrac * fullBarDegree / 360F;
 
-            if (displayFrac < dangerThreshold)
-                fillImage.color = dangerColor;
-            else if (displayFrac < warningThreshold)
-                fillImage.color = warningColor;
-            else
-                fillImage.color = normalColor;
+            UpdateValue();
 
         }
 
-        void Update()
+        protected override void UpdateValue()
         {
-            if (displayValue != curValue)
-            {
-                // Calculate new display value
-                displayValue = Mathf.MoveTowards(displayValue, curValue, smoothSpeed * Time.deltaTime);
-                
-                // Then update visuals
-                float displayFrac = displayValue / maxValue;
-                float displayDegree = displayFrac * fullBarDegree / 360F;
-                
-                fillImage!.fillAmount = displayDegree;
+            // Calculate new display value
+            displayValue = Mathf.MoveTowards(displayValue, curValue, smoothSpeed * Time.deltaTime);
+            
+            // Then update visuals
+            float displayFrac = displayValue / maxValue;
+            float displayDegree = displayFrac * fullBarDegree / 360F;
+            
+            fillImage!.fillAmount = displayDegree;
 
-                if (displayFrac < dangerThreshold)
-                    fillImage!.color = dangerColor;
-                else if (displayFrac < warningThreshold)
-                    fillImage!.color = warningColor;
-                else
-                    fillImage!.color = normalColor;
-
-            }
+            if (displayFrac < dangerThreshold)
+                fillImage!.color = dangerColor;
+            else if (displayFrac < warningThreshold)
+                fillImage!.color = warningColor;
+            else
+                fillImage!.color = normalColor;
             
         }
     }
