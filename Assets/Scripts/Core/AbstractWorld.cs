63d89fede62d5d21c0ac049348b15dcf363e907f
Tasuku Bobcorn <DevBobcorn@outlook.com>
2023-06-25 16:15:24 +0800

    :art: Tweak biome-related code

--- Assets/Scripts/Core/AbstractWorld.cs
+++ Assets/Scripts/Core/AbstractWorld.cs
@@ -13,11 +13,14 @@ namespace MinecraftClient.Mapping
         public static readonly int DEFAULT_GRASS   = 0x91BD59;
         public static readonly int DEFAULT_WATER   = 0x3F76E4;
 
-        public virtual float3 GetFoliageColor(Location loc) => DEFAULT_FOLIAGE;
+        public static readonly Biome DUMMY_BIOME = new(ResourceLocation.INVALID,
+                0, DEFAULT_FOLIAGE, DEFAULT_GRASS, DEFAULT_WATER, 0, 0);
 
-        public virtual float3 GetGrassColor(Location loc) => DEFAULT_GRASS;
+        public virtual float3 GetFoliageColor(Location loc) => DUMMY_BIOME.FoliageColor;
 
-        public virtual float3 GetWaterColor(Location loc) => DEFAULT_WATER;
+        public virtual float3 GetGrassColor(Location loc) => DUMMY_BIOME.GrassColor;
+
+        public virtual float3 GetWaterColor(Location loc) => DUMMY_BIOME.WaterColor;
 
     }
 }
