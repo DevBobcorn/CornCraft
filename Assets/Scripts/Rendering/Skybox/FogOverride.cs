32f09ab2c3110a9c7df392dcb60eb3a31bd8d516
Tasuku Bobcorn <DevBobcorn@outlook.com>
2023-10-29 09:17:42 +0800

    :sparkles: Replace UniStorm with BoatAttack environment

---
+++ Assets/Scripts/Rendering/Skybox/FogOverride.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class FogOverride : MonoBehaviour
+{
+    [ColorUsage(false, true)]
+    public Color mFogColor = Color.white;
+
+    private void LateUpdate()
+    {
+        RenderSettings.fogColor = mFogColor;
+    }
+}
