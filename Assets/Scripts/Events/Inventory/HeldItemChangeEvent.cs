3dbec18d266c961a3091e5614206f263317669de
DevBobcorn <devbobcorn@outlook.com>
2023-11-23 17:59:57 +0800

    :recycle: Tweak held item update handling and item actions

--- Assets/Scripts/Events/Inventory/HeldItemChangeEvent.cs
+++ Assets/Scripts/Events/Inventory/HeldItemChangeEvent.cs
@@ -1,15 +1,19 @@
 #nullable enable
+using CraftSharp.Control;
+
 namespace CraftSharp.Event
 {
     public record HeldItemChangeEvent : BaseEvent
     {
         public int HotbarSlot { get; }
         public ItemStack? ItemStack { get; }
+        public ItemActionType ActionType { get; }
 
-        public HeldItemChangeEvent(int hotbarSlot, ItemStack? itemStack)
+        public HeldItemChangeEvent(int hotbarSlot, ItemStack? itemStack, ItemActionType actionType)
         {
             HotbarSlot = hotbarSlot;
             ItemStack = itemStack;
+            ActionType = actionType;
         }
     }
 }
