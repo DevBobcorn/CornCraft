1f2d0a29660e8ee3297fa1b7ec99befbfc57788e
Tasuku <devbobcorn@outlook.com>
2024-02-19 21:58:32 +0800

    :construction: KKS model import util, phase II

--- Assets/MMD4Unity/Editor/MMDLoader/Private/PMXKKSConverter.cs
+++ Assets/MMD4Unity/Editor/MMDLoader/Private/PMXKKSConverter.cs
@@ -8,14 +8,11 @@ namespace MMD
 {
     public class PMXKKSConverter : PMXConverter
     {
-        private readonly bool isBaseModel;
-
         /// <summary>
         /// デフォルトコンストラクタ
         /// </summary>
         public PMXKKSConverter(bool isBaseModel)
         {
-            this.isBaseModel = isBaseModel;
         }
 
         /// <summary>
@@ -29,6 +26,8 @@ namespace MMD
         /// <param name='scale'>スケール</param>
         public override GameObject CreateGameObject(PMXFormat format, PhysicsType physics_type, AnimationType animation_type, bool use_ik, bool use_leg_d_bones, float scale)
         {
+            Debug.Log($"Creating prefab from {format.meta_header.folder}...");
+
             format_ = format;
             use_ik_ = use_ik;
             use_leg_d_bones_ = use_leg_d_bones;
@@ -37,16 +36,33 @@ namespace MMD
             MMDEngine engine = root_game_object_.AddComponent<MMDEngine>(); //MMDEngine追加
             //スケール・エッジ幅
             engine.scale = scale_;
+
+            //PMXファイルのインポート
+            try {
+                //PMX読み込みを試みる
+                Debug.Log($"Reading outfit data from {format_.meta_header.folder + "/Outfit 00/model.pmx"}...");
+                outfit_format_ = PMXLoaderScript.Import(format_.meta_header.folder + "/Outfit 00/model.pmx");
+            } catch (System.FormatException) {
+                Debug.LogWarning("Failed to read outfit pmx file.");
+            }
             
-            MeshCreationInfo[] creation_info = CreateMeshCreationInfo();                // メッシュを作成する為の情報を作成
-            Mesh[] mesh = CreateMesh(creation_info);                                    // メッシュの生成・設定
-            Material[][] materials = CreateMaterials(creation_info);                    // マテリアルの生成・設定
+            MeshCreationInfo[] creation_info = CreateMeshCreationInfo(format_);                                 // メッシュを作成する為の情報を作成
+            Mesh[] mesh = CreateMesh(format_, creation_info);                                                   // メッシュの生成・設定
+            Material[][] materials = CreateMaterials(format_, is_base_model: true, creation_info);              // マテリアルの生成・設定
 
-            Debug.Log("Mesh and materials created");
+            MeshCreationInfo[] o_creation_info = CreateMeshCreationInfo(outfit_format_);                        // メッシュを作成する為の情報を作成
+            Mesh[] o_mesh = CreateMesh(outfit_format_, o_creation_info, save_name: "outfit");                   // メッシュの生成・設定
+            Material[][] o_materials = CreateMaterials(outfit_format_, is_base_model: false, o_creation_info);  // マテリアルの生成・設定
 
-            GameObject[] bones = CreateBones();                                            // ボーンの生成・設定
-            SkinnedMeshRenderer[] renderers = BuildingBindpose(mesh, materials, bones);    // バインドポーズの作成
-            CreateMorph(mesh, materials, bones, renderers, creation_info);                 // モーフの生成・設定
+            GameObject[] bones = CreateBones();                                               // ボーンの生成・設定
+
+            // Outfit shares exactly same bones with base model
+            SkinnedMeshRenderer[] renderers = BuildingBindpose(mesh, materials, bones);       // バインドポーズの作成
+            SkinnedMeshRenderer[] o_renderers = BuildingBindpose(o_mesh, o_materials, bones); // バインドポーズの作成
+
+            o_renderers[0].transform.parent.gameObject.name = "Mesh (Outfit)";
+
+            CreateMorph(mesh, materials, bones, renderers, creation_info);                    // モーフの生成・設定(Base model only)
 
             // Fern NPR Face SDF helper script assigning [Code removed]
 
@@ -106,8 +122,10 @@ namespace MMD
         /// マテリアル作成
         /// </summary>
         /// <returns>マテリアル</returns>
+        /// <param name='mats_owner'>The format containing the materials to process. <br>This can be different from 'format_'.</param>
+        /// <param name='is_base_model'>True if base model, False if outfit.</param>
         /// <param name='creation_info'>メッシュ作成情報</param>
-        private Material[][] CreateMaterials(MeshCreationInfo[] creation_info)
+        private Material[][] CreateMaterials(PMXFormat mats_owner, bool is_base_model, MeshCreationInfo[] creation_info)
         {
             // 適当なフォルダに投げる
             string path = format_.meta_header.folder + "/Materials/";
@@ -116,8 +134,8 @@ namespace MMD
             }
             
             //全マテリアルを作成
-            Material[] materials = EntryAttributesForMaterials();
-            CreateAssetForMaterials(materials);
+            Material[] materials = EntryAttributesForMaterials(mats_owner, is_base_model);
+            CreateAssetForMaterials(mats_owner, materials);
 
             //メッシュ単位へ振り分け
             Material[][] result = new Material[creation_info.Length][];
@@ -164,14 +182,17 @@ namespace MMD
         /// マテリアルに基本情報(シェーダー・カラー・テクスチャ)を登録する
         /// </summary>
         /// <returns>マテリアル</returns>
-        private Material[] EntryAttributesForMaterials()
+        private Material[] EntryAttributesForMaterials(PMXFormat mats_owner, bool is_base_model)
         {
-            PMXBaseMaterialConverter materialConv = new PMXFernKKSMaterialConverter(root_game_object_, format_, scale_);
+            PMXBaseMaterialConverter materialConv = new PMXFernKKSMaterialConverter(root_game_object_, mats_owner, scale_);
             
-            return Enumerable.Range(0, format_.material_list.material.Length)
+            return Enumerable.Range(0, mats_owner.material_list.material.Length)
                     .Select(x => (uint) x)
-                    .Select(x => materialConv.Convert(x, IsTransparentByName(isBaseModel, format_.material_list.material[x].name)))
+                    .Select(x => materialConv.Convert(x, IsTransparentByName(is_base_model, mats_owner.material_list.material[x].name)))
                     .ToArray();
         }
+
+        // Character outfix is stored in another pmx file
+        protected PMXFormat outfit_format_;
     }
 }
