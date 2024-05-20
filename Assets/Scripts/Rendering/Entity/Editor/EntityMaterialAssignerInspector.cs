00ff82acf67f15327314f8363e1fd82c3f082bb4
DevBobcorn <devbobcorn@outlook.com>
2024-05-21 00:04:53 +0800

    :construction: Material assigner inspector

---
+++ Assets/Scripts/Rendering/Entity/Editor/EntityMaterialAssignerInspector.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace CraftSharp.Rendering.Editor
+{
+    [CustomEditor(typeof (EntityMaterialAssigner))]
+    public class EntityMaterialAssignerInspector : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var assigner = target as EntityMaterialAssigner;
+
+            // Draw all material groups
+            //EditorGUILayout.LabelField("MEOW");
+
+            // Draw all renderers
+            if (GUILayout.Button("Initialize"))
+            {
+                assigner.InitializeRenderers();
+            }
+
+            // Draw default inspector
+            base.OnInspectorGUI();
+        }
+    }
+}
---
+++ Assets/Scripts/Rendering/Entity/EntityMaterialAssigner.cs
@@ -0,0 +1,72 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace CraftSharp.Rendering
+{
+    [Serializable]
+    public class EntityMaterialEntry
+    {
+        [SerializeField] public EntityRenderType RenderType = EntityRenderType.SOLID;
+        [SerializeField] private Material? m_DefaultMaterial = null;
+        public Material DefaultMaterial => m_DefaultMaterial!;
+        [SerializeField] public string TextureId = string.Empty;
+        [SerializeField] private Renderer[] m_Renderers = { };
+        public Renderer[] Renderers => m_Renderers;
+
+        public EntityMaterialEntry(EntityRenderType renderType, Material material, Renderer[] renderers)
+        {
+            m_DefaultMaterial = material;
+            RenderType = renderType;
+
+            if (material != null && material.mainTexture != null)
+            {
+                TextureId = material.mainTexture.name;
+            }
+
+            m_Renderers = renderers;
+        }
+    }
+
+    public class EntityMaterialAssigner : MonoBehaviour
+    {
+        [SerializeField] private EntityMaterialEntry[] m_MaterialEntries = { };
+        
+        public EntityMaterialEntry[] MaterialEntries => m_MaterialEntries;
+
+        public void InitializeRenderers()
+        {
+            var renderers = gameObject.GetComponentsInChildren<Renderer>();
+            var entries = new Dictionary<Material, List<Renderer>>();
+
+            foreach (var renderer in renderers)
+            {
+                if (renderer.sharedMaterial == null) continue;
+
+                if (!entries.ContainsKey(renderer.sharedMaterial))
+                {
+                    entries.Add(renderer.sharedMaterial, new());
+                }
+                
+                entries[renderer.sharedMaterial].Add(renderer);
+            }
+
+            m_MaterialEntries = entries.Select(x => new EntityMaterialEntry(
+                    EntityRenderType.SOLID, x.Key, x.Value.ToArray() ) ).ToArray();
+        }
+
+        void Start()
+        {
+            var matManager = CornApp.CurrentClient!.EntityMaterialManager;
+
+            foreach (var entry in m_MaterialEntries)
+            {
+                var textureId = ResourceLocation.FromString(entry.TextureId);
+
+                var mat = matManager.MapMaterial(entry.RenderType, textureId, entry.DefaultMaterial);
+            }
+        }
+    }
+}
