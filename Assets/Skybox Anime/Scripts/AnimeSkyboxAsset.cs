936ad323d26a1359fe95166e3fb523bfbe17684a
Tasuku <devbobcorn@outlook.com>
2024-08-20 02:43:32 +0800

    :bug: Fix sun direction

--- Assets/Skybox Anime/Scripts/AnimeSkyboxAsset.cs
+++ Assets/Skybox Anime/Scripts/AnimeSkyboxAsset.cs
@@ -4,13 +4,13 @@ using UnityEngine.Playables;
 
 namespace AnimeSkybox
 {
-    public class MySkyboxAsset : PlayableAsset
+    public class AnimeSkyboxAsset : PlayableAsset
     {
         public List<MaterialProperty> properties; // 用于存储所有需要调整的属性
 
         public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
         {
-            var playable = ScriptPlayable<MySkyboxBehaviour>.Create(graph);
+            var playable = ScriptPlayable<AnimeSkyboxBehaviour>.Create(graph);
             var behaviour = playable.GetBehaviour();
 
             behaviour.properties = properties;
