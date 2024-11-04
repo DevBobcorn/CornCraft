b7d38f2f7f53ad3dbd48f9d256391b174f2105c6
Tasuku Bobcorn <DevBobcorn@outlook.com>
2024-11-04 23:14:32 +0800

    :sparkles: Handle entity velocity packets (not used yet)

--- Assets/Scripts/Client/CornClientOffline.cs
+++ Assets/Scripts/Client/CornClientOffline.cs
@@ -176,7 +176,7 @@ namespace CraftSharp
         public override string GetSessionId() => string.Empty;
         public override double GetServerTps() => 20;
         public override int GetPacketCount() => 0;
-        public override float GetTickMilSec() => 0.05F;
+        public override float GetTickMilSec() => 50F;
 
         /// <summary>
         /// Get current chunk render manager
