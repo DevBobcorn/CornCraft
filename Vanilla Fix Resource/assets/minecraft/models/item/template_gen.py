8184699bdda8caefac976716f52dddbf72747e11
Tasuku Bobcorn <DevBobcorn@outlook.com>
2025-04-12 22:54:20 +0800

    :bug: Fix model for bed and banner items

---
+++ Vanilla Fix Resource/assets/minecraft/models/item/template_gen.py
@@ -0,0 +1,18 @@
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+os.chdir(script_dir)
+
+colors = [
+    'white', 'orange', 'magenta', 'light_blue', 'yellow',
+    'lime', 'pink', 'gray', 'light_gray', 'cyan', 'purple',
+    'blue', 'brown', 'green', 'red', 'black'
+]
+
+with open('template_gen.json') as f:
+    txt = f.read()
+
+    for color in colors:
+        with open(f'{color}_banner.json', 'w+') as fo:
+            fo.write(txt.replace('COLOR', color))
