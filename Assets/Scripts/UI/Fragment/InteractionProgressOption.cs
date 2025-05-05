2b81be8e697bf479f32babb4dd02650efc64fd2c
Tasuku Bobcorn <DevBobcorn@outlook.com>
2025-05-05 21:48:35 +0800

    :lipstick: And also remake interaction options using new shader

--- Assets/Scripts/UI/Fragment/InteractionProgressOption.cs
+++ Assets/Scripts/UI/Fragment/InteractionProgressOption.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CraftSharp.UI
 {
     [RequireComponent(typeof (Animator))]
     public class InteractionProgressOption : InteractionOption
     {
-        [SerializeField] private RectTransform progressFill;
+        private static readonly int FILL_AMOUNT  = Shader.PropertyToID("_FillAmount");
+        private static readonly int DELTA_AMOUNT = Shader.PropertyToID("_DeltaAmount");
+        
+        [SerializeField] private Image barImage;
+        private Material barMaterial;
+        
+        private void Start()
+        {
+            // Create a material instance for each bar
+            barMaterial = new Material(barImage.material);
+            barImage.material = barMaterial;
+            
+            UpdateProgress(0F);
+        }
 
         public void UpdateProgress(float progress)
         {
-            var parentWidth = ((RectTransform) progressFill.parent).rect.width;
-
-            progressFill.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, parentWidth * progress);
+            barMaterial.SetFloat(FILL_AMOUNT, progress);
+            barMaterial.SetFloat(DELTA_AMOUNT, progress);
         }
     }
 }
