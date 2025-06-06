a7f01fba97ebeadeeab0b80835c0af973f28c313
Tasuku Bobcorn <DevBobcorn@outlook.com>
2025-06-06 22:00:56 +0800

    :bento: Update builtin asset zip

--- Assets/Scripts/Client/CornApp.cs
+++ Assets/Scripts/Client/CornApp.cs
@@ -31,7 +31,7 @@ namespace CraftSharp
         private const int EDITOR_FPS_LIMIT = 60;
 
         public const string CORN_CRAFT_BUILTIN_FILE_NAME = "CornCraftBuiltin";
-        public const int    CORN_CRAFT_BUILTIN_VERSION = 26;
+        public const int    CORN_CRAFT_BUILTIN_VERSION = 27;
         private const string VANILLA_FIX_FILE_NAME = "VanillaFix";
         private const int    VANILLA_FIX_VERSION = 6;
         
