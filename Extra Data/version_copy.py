e0098dbe878a6908866ee63adc92547da79a8166
Tasuku Bobcorn <DevBobcorn@outlook.com>
2025-06-08 20:38:19 +0800

    :bento: Add enchantment definitions for all versions

--- Extra Data/version_copy.py
+++ Extra Data/version_copy.py
@@ -4,7 +4,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 os.chdir(script_dir)
 
-# inventory_types
+# inventories/inventory_types
 # 1.16.5 -> 1.17.1
 #        -> 1.18.2
 #        -> 1.19.2
@@ -18,12 +18,36 @@ versions = {
     '1.20.4': ['1.20.6']
 }
 
-for (src, dsts) in versions.items():
-    for dst in dsts:
-        s = f'./inventories/inventory_types-{src}.json'
-        d = f'./inventories/inventory_types-{dst}.json'
+if False:
+    for (src, dsts) in versions.items():
+        for dst in dsts:
+            s = f'./inventories/inventory_types-{src}.json'
+            d = f'./inventories/inventory_types-{dst}.json'
 
-        print(f'{s} -> {d}')
+            print(f'{s} -> {d}')
 
-        shutil.copy(s, d)
+            shutil.copy(s, d)
 
+# items/enchantment_types
+# 1.16.5 -> 1.17.1
+#        -> 1.18.2
+# 1.19.2 -> 1.19.3
+#        -> 1.19.4
+#        -> 1.20.1
+#        -> 1.20.2
+#        -> 1.20.4
+# 1.20.6
+versions = {
+    '1.16.5': ['1.17.1', '1.18.2'],
+    '1.19.2': ['1.19.3', '1.19.4', '1.20.1', '1.20.2', '1.20.4']
+}
+
+if True:
+    for (src, dsts) in versions.items():
+        for dst in dsts:
+            s = f'./items/enchantment_types-{src}.json'
+            d = f'./items/enchantment_types-{dst}.json'
+
+            print(f'{s} -> {d}')
+
+            shutil.copy(s, d)
