88cf2bfe5631c690dc3192e8a2bc1a5b99d74ae0
Tasuku Bobcorn <DevBobcorn@outlook.com>
2025-07-20 15:25:03 +0800

    :bug: Fix item stack predicate

--- Packages/com.devbobcorn.craftsharp-protocol/Runtime/Scripts/Inventory/ItemStackPredicate.cs
+++ Packages/com.devbobcorn.craftsharp-protocol/Runtime/Scripts/Inventory/ItemStackPredicate.cs
@@ -6,11 +6,17 @@ namespace CraftSharp.Inventory
 {
     public class ItemStackPredicate
     {
+        public enum Operator
+        {
+            EQUAL,
+            NOT_EQUAL
+        }
+        
         public static readonly ItemStackPredicate EMPTY = new(new());
         
-        private readonly Dictionary<string, string> conditions;
+        private readonly Dictionary<string, (Operator, string)> conditions;
 
-        public ItemStackPredicate(Dictionary<string, string> conditions)
+        public ItemStackPredicate(Dictionary<string, (Operator, string)> conditions)
         {
             this.conditions = conditions;
         }
@@ -20,14 +26,26 @@ namespace CraftSharp.Inventory
             if (source is "" or "normal")
                 return EMPTY;
             
-            var conditions = new Dictionary<string, string>();
+            var conditions = new Dictionary<string, (Operator, string)>();
             var srcs = source.Split(',');
             foreach (var src in srcs)
             {
-                if (src.Contains('='))
+                string[] keyVal;
+                
+                if (src.Contains("=="))
                 {
-                    var keyVal = src.Split('=', 2);
-                    conditions.Add(keyVal[0], keyVal[1]);
+                    keyVal = src.Split("==", 2);
+                    conditions.Add(keyVal[0], (Operator.EQUAL, keyVal[1]));
+                }
+                else if (src.Contains("!="))
+                {
+                    keyVal = src.Split("!=", 2);
+                    conditions.Add(keyVal[0], (Operator.NOT_EQUAL, keyVal[1]));
+                }
+                else if (src.Contains('='))
+                {
+                    keyVal = src.Split('=', 2);
+                    conditions.Add(keyVal[0], (Operator.EQUAL, keyVal[1]));
                 }
                 else
                 {
@@ -37,63 +55,91 @@ namespace CraftSharp.Inventory
             return new ItemStackPredicate(conditions);
         }
 
+        private static bool CheckIsCondition(string itemPredicate, ItemStack itemStack)
+        {
+            return itemPredicate switch
+            {
+                "damageable" => itemStack.IsDamageable,
+                "stackable" => itemStack.IsStackable,
+                "enchanted" => itemStack.IsEnchanted,
+                "empty" => itemStack.IsEmpty,
+                _ => throw new System.IO.InvalidDataException($"Undefined item predicate: {itemPredicate}")
+            };
+        }
+
         public bool Check(ItemStack itemStack)
         {
-            foreach (var (key, value) in conditions)
+            foreach (var (key, (op, value)) in conditions)
             {
                 // Multiple allowed values are supported, separated with symbol '|'
                 var allowedValues = value.Split('|');
+                bool conditionMet = false;
 
                 switch (key)
                 {
                     case "id":
-                        if (!allowedValues.Select(ResourceLocation.FromString)
-                                .Contains(itemStack.ItemType.ItemId))
                         {
-                            return false;
+                            var itemId = itemStack.ItemType.ItemId.ToString();
+                            if (op == Operator.EQUAL)
+                                conditionMet = allowedValues.Any(x => x == itemId);
+                            if (op == Operator.NOT_EQUAL)
+                                conditionMet = allowedValues.All(x => x != itemId);
                         }
                         break;
                     case "is":
-                        return allowedValues.Any(x =>
                         {
-                            var res = x switch
-                            {
-                                "damageable" => itemStack.IsDamageable,
-                                "stackable" => itemStack.IsStackable,
-                                "enchanted" => itemStack.IsEnchanted,
-                                "empty" => itemStack.IsEmpty,
-                                _ => throw new System.IO.InvalidDataException($"Undefined item predicate: {x}")
-                            };
-                            
-                            Debug.Log($"{x}: {res}");
-
-                            return res;
-                        });
+                            if (op == Operator.EQUAL)
+                                conditionMet = allowedValues.Any(x => CheckIsCondition(x, itemStack));
+                            if (op == Operator.NOT_EQUAL)
+                                conditionMet = allowedValues.All(x => !CheckIsCondition(x, itemStack));
+                        }
+                        break;
                     case "id_path_starts_with":
-                        if (!allowedValues.Any(x => itemStack.ItemType.ItemId.Path.StartsWith(x)))
                         {
-                            return false;
+                            var itemIdPath = itemStack.ItemType.ItemId.Path;
+                            if (op == Operator.EQUAL)
+                                conditionMet = allowedValues.Any(x => itemIdPath.StartsWith(x));
+                            if (op == Operator.NOT_EQUAL)
+                                conditionMet = allowedValues.All(x => !itemIdPath.StartsWith(x));
                         }
                         break;
                     case "id_path_ends_with":
-                        if (!allowedValues.Any(x => itemStack.ItemType.ItemId.Path.EndsWith(x)))
                         {
-                            return false;
+                            var itemIdPath = itemStack.ItemType.ItemId.Path;
+                            if (op == Operator.EQUAL)
+                                conditionMet = allowedValues.Any(x => itemIdPath.EndsWith(x));
+                            if (op == Operator.NOT_EQUAL)
+                                conditionMet = allowedValues.All(x => !itemIdPath.EndsWith(x));
+
+                            var o = op switch
+                            {
+                                Operator.EQUAL => "ends",
+                                Operator.NOT_EQUAL => "does not end",
+                                _ => "WTF"
+                            };
+                            Debug.Log($"Checking if {itemIdPath} {o} with any of {string.Join(", ", allowedValues)}. Result: {conditionMet}");
                         }
                         break;
                     case "equipment_slot":
-                        if (!allowedValues.Select(EquipmentSlotHelper.GetEquipmentSlot)
-                                .Contains(itemStack.ItemType.EquipmentSlot))
-                        {
-                            return false;
-                        }
+                    {
+                        if (op == Operator.EQUAL)
+                            conditionMet = allowedValues.Select(EquipmentSlotHelper.GetEquipmentSlot)
+                                .Any(x => x == itemStack.ItemType.EquipmentSlot);
+                        if (op == Operator.NOT_EQUAL)
+                            conditionMet = allowedValues.Select(EquipmentSlotHelper.GetEquipmentSlot)
+                                .All(x => x != itemStack.ItemType.EquipmentSlot);
+                    }
                         break;
                     default:
                         Debug.LogWarning($"Unknown condition key: {key}");
-                        return false;
+                        break;
                 }
+                
+                if (!conditionMet) // This condition is not met
+                    return false;
             }
 
+            // All conditions are met
             return true;
         }
     }
