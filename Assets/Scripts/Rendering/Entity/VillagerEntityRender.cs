568bcba62f0c3a4dccf3ed9e269f2f5f18c279c8
MolassesLover <mail@molasses.love>
2025-11-24 14:07:15 +0100

    🍱 Add villager entity model

---
+++ Assets/Scripts/Rendering/Entity/VillagerEntityRender.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace CraftSharp.Rendering
+{
+    public class VillagerEntityRender : BipedEntityRender
+    {
+        public override void UpdateAnimation(float tickMilSec)
+        {
+            base.UpdateAnimation(tickMilSec);
+        }
+    }
+}
